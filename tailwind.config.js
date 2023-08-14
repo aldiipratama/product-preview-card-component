12dbee101440ebd296d26ae46d4d88fdfe6352c8
aldiipratama <paldi0013@gmail.com>
2023-08-14 21:46:35 +0700

    challange from frontend mentor

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ["'Montserrat'"],
+        fraunces: ["'Fraunces'"],
+      },
+    },
+  },
+  plugins: [],
+};
